395911e3052079f4fff18613589024922ea96598
Julian <julian.pedro@hotmail.com>
2020-09-22 23:43:55 -0400

    Teste Sonar

--- app/models.py
+++ app/models.py
@@ -7,5 +7,9 @@ Copyright (c) 2019 - present AppSeed.us
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Test(object):
+    """docstring for Test."""
+    test = models.CharField(verbose_name='Teste')
 
+    def is_valid(self):
+        return true
